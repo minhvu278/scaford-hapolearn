0101da4b033ee184af092256905c49d3040bf22c
vudo <dominhvu278@gmail.com>
2022-06-17 09:16:32 +0700

    code banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,25 @@
 
 @section('content')
 
-<section class="example">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-                    <div class="card-body">
-                        Hello World!!!
-                    </div>
-                </div>
-            </div>
+<section class="banner">
+    <div class="banner-content">
+        <div class="banner-heading">
+            <p class="banner-slogan">
+                Learn Anytime, Anywhere
+                <span class="slogan-title">at Hapo Learn <img src="{{ asset('images/cu.png') }}" alt="At Hapo"> !</span>
+            </p>
         </div>
+
+        <div class="banner-text">
+            <p class="paragraph">
+                Interactive lessons, "on-the-go"
+                practice, peer support.
+            </p>
+        </div>
+
+        <button class="banner-button">
+            Start Learning Now!
+        </button>
     </div>
 </section>
 @endsection
