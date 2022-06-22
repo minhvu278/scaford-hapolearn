866e9d3f20cbce523b8f086b2e0ce3d6fd2a74bd
vudo <dominhvu278@gmail.com>
2022-06-22 14:42:16 +0700

    #3 fix space scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-
-<section class="banner">
-    <div class="banner-content">
-        <div class="banner-heading">
-            <p class="banner-slogan">
-                Learn Anytime, Anywhere
-                <span class="slogan-title">at Hapo Learn <img src="{{ asset('images/cu.png') }}" alt="At Hapo"> !</span>
-            </p>
+    <section class="container-fluid position-relative banner">
+        <div class="position-absolute">
+            <div class="row">
+                <div class="col-4 col-sm">
+                    <div class="banner-heading">
+                        <p class="banner-slogan">
+                            Learn Anytime, Anywhere
+                            <span class="slogan-title">at Hapo Learn <img class="banner-cu"
+                                                                          src="{{ asset('images/cu.png') }}"
+                                                                          alt="At Hapo"> !</span>
+                        </p>
+                    </div>
+                    <div class="banner-text">
+                        <p class="paragraph">
+                            Interactive lessons, "on-the-go"
+                            practice, peer support.
+                        </p>
+                    </div>
+                    <button class="banner-button">
+                        Start Learning Now!
+                    </button>
+                </div>
+            </div>
         </div>
-
-        <div class="banner-text">
-            <p class="paragraph">
-                Interactive lessons, "on-the-go"
-                practice, peer support.
-            </p>
-        </div>
-
-        <button class="banner-button">
-            Start Learning Now!
-        </button>
-    </div>
-</section>
+    </section>
 @endsection
+
+
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -13,15 +13,13 @@
                     <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                    </a>
+                    <a class="nav-link" href="#">ALL COURSES</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
+                    <a class="nav-link" href="#">LOGIN/REGISTER</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PROFILE</a>
                 </li>
             </ul>
         </div>
