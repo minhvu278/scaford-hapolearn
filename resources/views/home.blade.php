0f70c0cb2149488b2743320908c70238745218b9
vudo <dominhvu278@gmail.com>
2022-06-28 17:15:35 +0700

    #9 Code header banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,24 @@
 @extends('layouts.app')
-
 @section('content')
-
-<section class="example">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-                    <div class="card-body">
-                        Hello World!!!
+    <section class="banner d-flex align-items-center">
+        <div class="container-fluid banner-wrap">
+            <div class="row banner-heading">
+                <div class="col-lg-5 col-md-6 ml-3 col-sm-12 ">
+                    <p class="banner-title">Learn Anytime, Anywhere
+                        <span class="banner-title title-weight">at HapoLearn
+                    <img class="banner-image" src="{{ asset('images/owl.png') }}"
+                         alt="HapoLearn Logo"> !
+                </span>
+                    </p>
+                    <div class="row">
+                        <p class="col-lg-6 col-md-7 col-sm-3 paragraph">Interactive lessons, "on-the-go"
+                            practice, peer support.
+                        </p>
                     </div>
+                    <button class="banner-button"> Start Learning Now!</button>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+    <div class="bg-gray"></div>
 @endsection
