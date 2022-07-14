f9de21d0476dc28d281d43bad1ca817122565fdc
vudo <dominhvu278@gmail.com>
2022-07-14 08:43:02 +0700

    #21 Fix space

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,97 +1,97 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center login-location">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header login-title">{{ __('Sign in to HapoLearn') }}</div>
+<div class="container">
+    <div class="row justify-content-center login-location">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header login-title">{{ __('Sign in to HapoLearn') }}</div>
 
-                    @if($errors->any())
-                        <div class="alert alert-danger text-center">
-                            Dữ liệu nhập vào không khớp, vui lòng kiểm tra lại
+                @if($errors->any())
+                    <div class="alert alert-danger text-center">
+                        Dữ liệu nhập vào không khớp, vui lòng kiểm tra lại
+                    </div>
+                @endif
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <div class="col-md-12 d-flex">
+                                <label for="name"
+                                        class="col-md-4 col-form-label text-md-left p-0 login-label">{{ __('Username') }}</label>
+                            </div>
+                            <div class="col-md-12">
+                                <input id="name" type="text"
+                                        class="form-control @error('name') is-invalid @enderror login-input"
+                                        name="name"
+                                        value="{{ old('name') }}" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
                         </div>
-                    @endif
+                        <div class="form-group row">
+                            <div class="col-md-12 d-flex">
+                                <label for="password"
+                                        class="col-md-4 col-form-label text-md-left p-0 login-label">{{ __('Password') }}</label>
+                            </div>
+                            <div class="col-md-12">
+                                <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror login-input"
+                                        name="password">
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-12 d-flex justify-content-between">
+                                <button type="submit" class="btn btn-submit">
+                                    {{ __('Sign in') }}
+                                </button>
 
-                            <div class="form-group row">
-                                <div class="col-md-12 d-flex">
-                                    <label for="name"
-                                           class="col-md-4 col-form-label text-md-left p-0 login-label">{{ __('Username') }}</label>
-                                </div>
-                                <div class="col-md-12">
-                                    <input id="name" type="text"
-                                           class="form-control @error('name') is-invalid @enderror login-input"
-                                           name="name"
-                                           value="{{ old('name') }}" autofocus>
-
-                                    @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link forgot-password" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password') }}
+                                    </a>
+                                @endif
                             </div>
-                            <div class="form-group row">
-                                <div class="col-md-12 d-flex">
-                                    <label for="password"
-                                           class="col-md-4 col-form-label text-md-left p-0 login-label">{{ __('Password') }}</label>
-                                </div>
-                                <div class="col-md-12">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror login-input"
-                                           name="password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
+                        </div>
+                        <div class="form-group row justify-content-center">
+                            <div class="col-md-10 sign-with">
+                                <p class="title-google">Sign in with</p>
                             </div>
-                            <div class="form-group row mb-0">
-                                <div class="col-md-12 d-flex justify-content-between">
-                                    <button type="submit" class="btn btn-submit">
-                                        {{ __('Sign in') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link forgot-password" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="form-group row justify-content-center">
-                                <div class="col-md-10 sign-with">
-                                    <p class="title-google">Sign in with</p>
-                                </div>
-                            </div>
-                            <div class="form-group row justify-content-center mt-4 mb-4">
-                                <div class="col-md-10">
-                                    <button class="account-google">
-                                        <i class="fa-brands fa-google-plus-g"></i>
-                                        Google
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="form-group row justify-content-center">
-                                <div class="col-md-10 sign-with model-register">
-                                    <p class="title-google title-register">or New to HapoLearn</p>
-                                </div>
-                            </div>
-                            <div class="from-group row mt-5 justify-content-center">
-                                <button type="submit" class="btn btn-register">
-                                    {{ __('Create New Account') }}
+                        </div>
+                        <div class="form-group row justify-content-center mt-4 mb-4">
+                            <div class="col-md-10">
+                                <button class="account-google">
+                                    <i class="fa-brands fa-google-plus-g"></i>
+                                    Google
                                 </button>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+                        <div class="form-group row justify-content-center">
+                            <div class="col-md-10 sign-with model-register">
+                                <p class="title-google title-register">or New to HapoLearn</p>
+                            </div>
+                        </div>
+                        <div class="from-group row mt-5 justify-content-center">
+                            <button type="submit" class="btn btn-register">
+                                {{ __('Create New Account') }}
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
