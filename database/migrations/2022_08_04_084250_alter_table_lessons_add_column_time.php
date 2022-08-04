d115dc4fa4f1fd61837168906e10df85049217ae
vudo <dominhvu278@gmail.com>
2022-08-04 15:47:06 +0700

    #30 add alert table migration

--- database/migrations/2022_08_04_084250_alter_table_lessons_add_column_time.php
+++ database/migrations/2022_08_04_084250_alter_table_lessons_add_column_time.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class AlterTableLessonsAddColumnTime extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->integer('course_id');
-            $table->string('name')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->integer('time')->nullable();
         });
     }
 
@@ -29,6 +25,8 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->dropColumn('time');
+        });
     }
 }
